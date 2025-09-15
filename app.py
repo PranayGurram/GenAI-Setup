e27d625d27fe66759ab60700cdc56956068a1ec7
Gurram Pranay <145985144+PranayGurram@users.noreply.github.com>
2025-09-15 13:18:12 +0530

    Add Files

---
+++ app.py
@@ -0,0 +1,18 @@
+import google.generativeai as genai
+import streamlit as st
+import os
+
+# Get API key from environment variable
+key=os.getenv('GOOGLE_API_KEY')
+
+# Configure the model
+genai.configure(api_key=key)
+model=genai.GenerativeModel('gemini-2.5-flash-lite')
+
+# Create a frontend UI
+st.title("SIMPLE TEXT GENERATION")
+st.header('This is to test the gemini model as application')
+prompt=st.text_input('Write your prompt here',max_chars=10000)
+if prompt:
+    response = model.generate_content(prompt)
+    st.write(response.text)
